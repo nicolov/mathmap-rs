de31f0775216969cdce7841c0fcd615167b4c7bb
nicolov <nicolov@users.noreply.github.com>
2025-09-11 10:55:55 +0100

    Add moire_2 test

---
+++ examples/render/moire_2.mm
@@ -0,0 +1,4 @@
+filter moire2 ()
+	zoom = 100;
+    grayColor(sin(x*y*zoom+t*2*pi)*0.5+0.5)
+end
--- tests/render_tests.rs
+++ tests/render_tests.rs
@@ -35,6 +35,14 @@ fn render_moire1() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
+fn render_moire2() -> Result<(), Box<dyn std::error::Error>> {
+    let mut im = mathmap::exec_mathmap_script("examples/render/moire_2.mm", 256, 256, 1)?;
+    let im_ref = image::open("tests/render_moire_2.png")?.to_rgba8();
+    assert_eq!(im.next().unwrap(), im_ref.into());
+    Ok(())
+}
+
+#[test]
 fn render_spiral() -> Result<(), Box<dyn std::error::Error>> {
     let mut im = mathmap::exec_mathmap_script("examples/render/spiral.mm", 256, 256, 1)?;
     let im_ref = image::open("tests/render_spiral.png")?.to_rgba8();
