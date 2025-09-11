a9625e8581ee046d347b24fcb9974e1e35cb0e14
nicolov <nicolov@users.noreply.github.com>
2025-09-11 10:51:31 +0100

    Spiral test passes now

--- tests/render_tests.rs
+++ tests/render_tests.rs
@@ -1,3 +1,14 @@
+fn _max_pixel_diff(
+    lhs: &image::ImageBuffer<image::Rgba<u8>, Vec<u8>>,
+    rhs: &image::ImageBuffer<image::Rgba<u8>, Vec<u8>>,
+) -> u8 {
+    lhs.iter()
+        .zip(rhs.iter())
+        .map(|(a, b)| a.abs_diff(*b))
+        .max()
+        .unwrap()
+}
+
 #[test]
 fn render_gray() -> Result<(), Box<dyn std::error::Error>> {
     let mut im = mathmap::exec_mathmap_script("examples/render/gray.mm", 256, 256, 1)?;
@@ -17,34 +28,16 @@ fn render_grid() -> Result<(), Box<dyn std::error::Error>> {
 #[test]
 #[ignore]
 fn render_moire1() -> Result<(), Box<dyn std::error::Error>> {
-    let im = mathmap::exec_mathmap_script("examples/render/moire_1.mm", 256, 256, 1)?
-        .next()
-        .unwrap();
+    let mut im = mathmap::exec_mathmap_script("examples/render/moire_1.mm", 256, 256, 1)?;
     let im_ref = image::open("tests/render_moire_1.png")?.to_rgba8();
-    // assert_eq!(im.next().unwrap(), im_ref.into());
-    let max_diff = im
-        .iter()
-        .zip(im_ref.iter())
-        .map(|(a, b)| a.abs_diff(*b))
-        .max()
-        .unwrap();
-    println!("max diff: {}", max_diff);
+    assert_eq!(im.next().unwrap(), im_ref.into());
     Ok(())
 }
 
 #[test]
-#[ignore]
 fn render_spiral() -> Result<(), Box<dyn std::error::Error>> {
-    let im = mathmap::exec_mathmap_script("examples/render/spiral.mm", 256, 256, 1)?
-        .next()
-        .unwrap();
+    let mut im = mathmap::exec_mathmap_script("examples/render/spiral.mm", 256, 256, 1)?;
     let im_ref = image::open("tests/render_spiral.png")?.to_rgba8();
-    let max_diff = im
-        .iter()
-        .zip(im_ref.iter())
-        .map(|(a, b)| a.abs_diff(*b))
-        .max()
-        .unwrap();
-    println!("max diff: {}", max_diff);
+    assert_eq!(im.next().unwrap(), im_ref.into());
     Ok(())
 }
